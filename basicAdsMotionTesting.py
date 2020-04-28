b0350ff49f3ae4b9e1330a2c870bf73353b8a423
SCoopDawg <simon.cooper@stfc.ac.uk>
2020-04-28 09:31:04 +0100

    Move function definitions in to module, simplify test call and demo running through different position tests

--- basicAdsMotionTesting.py
+++ basicAdsMotionTesting.py
@@ -1,53 +1,45 @@
-def moveAbsolute(axisNum,position,velocity):
-    print(f"Move absolute on Axis {axisNum} to position {position} @ velocity {velocity}")
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stControl.eCommand",0, pyads.PLCTYPE_INT)
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stConfig.fVelocity",velocity, pyads.PLCTYPE_LREAL)
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stConfig.fPosition",position, pyads.PLCTYPE_LREAL)
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stControl.bExecute",True, pyads.PLCTYPE_BOOL)
 
-def axisOn(axisNum):
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stControl.bEnable",True, pyads.PLCTYPE_BOOL)
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stConfig.fOverride",100, pyads.PLCTYPE_LREAL)
-
-def axisOff(axisNum):
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stControl.bEnable",False, pyads.PLCTYPE_BOOL)
-    plc.write_by_name(f"GVL.astAxes[{axisNum}].stConfig.fOverride",0, pyads.PLCTYPE_LREAL)
-
-def waitForDone(axisNum, timeout=10):
-    timeLimit = time.time()+timeout
-    boolTimeoutError = False;
-    while(True):
-        if(plc.read_by_name(f"GVL.astAxes[{axisNum}].stStatus.bBusy", pyads.PLCTYPE_BOOL)):
-            break
-        if time.time()>timeLimit:
-            boolTimeoutError = True
-            break
-    while(True):
-        if(plc.read_by_name(f"GVL.astAxes[{axisNum}].stStatus.bDone", pyads.PLCTYPE_BOOL)):
-            break
-        if time.time()>timeLimit:
-            boolTimeoutError = True
-            break   
-    if boolTimeoutError:
-        print("Timeout error")
+def absolutePositionTest(plc,axisNum,targetPosition,velocity,threshold,timeout=10):
+    mf.moveAbsolute(plc,axisNum,targetPosition,velocity)
+    if mf.waitForDone(plc,axisNum,timeout) == False:
+        print("Timeout on absolute move")
+        return False
+    actualPosition = mf.readAxisPosition(plc,axisNum)
+    posDeviation = abs(targetPosition - actualPosition)
+    print('Target position: {:.3f}'.format(targetPosition))
+    print('Position reached: {:.3f}'.format(actualPosition))
+    print('Position deviation: {:.3f}'.format(posDeviation))
+    
+    if posDeviation>threshold:
+        print('Test failed')
+        print('\n********************\n')
+        return False
     else:
-        print("Done")
+        print('Test passed')
+        print('\n********************\n')
+        
+    
 
-def readAxisPosition(axisNum):
-    return plc.read_by_name(f"GVL.astAxes[{axisNum}].stStatus.fActPosition",pyads.PLCTYPE_LREAL)
 
 import pyads
 import time
+import plcMotionFunctions as mf
+from os import system, name
 # add remote route
+system('cls')
 plc = pyads.Connection('5.65.74.200.1.1',852) #852 is the port of the PLC, first value is AMS net ID
 plc.open()
-axisOn(1)
-moveAbsolute(1,2,2)
-waitForDone(1)
-print('Current position: {:.3f}'.format(readAxisPosition(1)))
-time.sleep(0.5)
-moveAbsolute(1,0,2)
-waitForDone(1)
-print('Current position: {:.3f}'.format(readAxisPosition(1)))
+mf.axisOn(plc, 1)
+#need to actually wait for the axis to turn on, it's a bit slow
+time.sleep(1)
+
+positionTargetsList = [2,0,1,2,1,2,0,3,0]
+
+for i in positionTargetsList:
+    absolutePositionTest(plc,1,i,2,0.005)
+    time.sleep(1)
+
+mf.axisOff(plc, 1)
+
 plc.close()
 
